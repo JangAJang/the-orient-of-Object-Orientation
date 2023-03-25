796e8858d4f81f72a3514f1d22b5c81711a99724
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-25 21:47:04 +0900

    feat: pt1에 대한 실행 클래스로 Client, AuthService, Member 객체간의 협동으로 로그인 기능이 구현되는 것을 검증하는 출력문과, 실행 기능 구현

--- chapter1/pt1/Client.java
+++ chapter1/pt1/Client.java
@@ -8,4 +8,9 @@ public class Client {
     public String[] requestLogIn(){
         return new String[]{id, password};
     }
+
+    public void getReturn(String returned){
+        System.out.println("사용자는 AuthService에게 요청에 대한 응답을 받는다.");
+        System.out.println(returned);
+    }
 }
--- chapter1/pt1/Member.java
+++ chapter1/pt1/Member.java
@@ -14,6 +14,8 @@ public class Member {
     }
 
     public boolean isRightPassword(String password){
+        System.out.println("AuthService는 사용자를 찾아 비밀번호를 검증한다.");
+        System.out.println("Member는 AuthService에게 비밀번호가 맞는지 그 응답을 반환한다.");
         return this.password.equals(password);
     }
 }
