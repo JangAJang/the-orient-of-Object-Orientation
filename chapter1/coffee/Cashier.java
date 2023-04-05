8c6ce9106ad414d316707b3212190476afe94558
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-05 15:41:16 +0900

    [refactor] Coffees타입 coffees를 바리스타가 책임지게 하고, 주문과 커피를 비교 검증하며 주문에 대한 책임만을 가지도록 캐셔 객체 리펙토링

--- chapter1/coffee/Cashier.java
+++ chapter1/coffee/Cashier.java
@@ -3,7 +3,6 @@ package chapter1.coffee;
 public class Cashier {
 
     private final Coffees orders = new Coffees();
-    private final Coffees coffees = new Coffees();
 
     public void getOrderFromCustomer(String order){
         orders.addNewCoffee(order);
@@ -15,10 +14,9 @@ public class Cashier {
 
     public void getCoffeeFromBarista(String coffee){
         if(!orders.isRightCoffee(coffee)) throw new IllegalArgumentException("커피 잘못만들었잖아!");
-        coffees.addNewCoffee(orders.offerCoffee());
     }
 
     public String giveCoffeeToCustomer(){
-        return coffees.offerCoffee();
+        return orders.offerCoffee();
     }
 }
