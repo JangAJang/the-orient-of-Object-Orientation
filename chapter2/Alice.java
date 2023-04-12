34f8a46c26bf49286c62f2d8bf034aec290fe1d9
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-12 14:54:52 +0900

    [feat] 기본적으로 케이크를 먹고, 차를 마시며, 부채를 쓰고, 버섯을 먹는 일을 하는 객체 앨리스 생성

---
+++ chapter2/Alice.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+public class Alice {
+
+    private void eatCake(){
+
+    }
+
+    private void drinkTea(){
+
+    }
+
+    private void useFan(){
+
+    }
+
+    private void eatMushroomToBeTall(){
+
+    }
+
+    private void eatMushroomToBeSmall(){
+
+    }
+}
