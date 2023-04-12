aba6c4c742f408aeed34b9448b85739dda0b5a08
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-12 15:10:42 +0900

    [feat] 앨리스가 먹을 케이크나 차, 버섯이 없을 경우 예외를 반환하는 로직 구현

--- chapter2/Alice.java
+++ chapter2/Alice.java
@@ -16,14 +16,18 @@ public class Alice {
         if(cake.isLeft()){
             cake.removePortion();
             height *= 2;
+            return;
         }
+        throw new IllegalArgumentException("케이크가 없습니다.");
     }
 
     private void drinkTea(){
         if(tea.isLeft()){
             height = 24;
             tea.drunken();
+            return;
         }
+        throw new IllegalArgumentException("차가 없습니다.");
     }
 
     private void useFan(){
@@ -34,13 +38,17 @@ public class Alice {
         if(mushroom.isLeftLeft()){
             mushroom.leftEaten();
             height += 10;
+            return;
         }
+        throw new IllegalArgumentException("더 커지기 위해 먹을 버섯이 없습니다.");
     }
 
     private void eatMushroomToBeSmall(){
         if(mushroom.isRightLeft()){
             mushroom.rightEaten();
             height -= 10;
+            return;
         }
+        throw new IllegalArgumentException("더 작아지기 위해 먹을 버섯이 없습니다.");
     }
 }
