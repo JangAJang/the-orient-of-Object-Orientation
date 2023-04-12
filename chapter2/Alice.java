16247e208b54bf2e406bdff8e43040db3f7a02c9
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-12 15:03:28 +0900

    [feat] 앨리스가 먹고, 사용하는 객체 케이크, 차, 버섯, 부채 객체 생성

--- chapter2/Alice.java
+++ chapter2/Alice.java
@@ -2,23 +2,34 @@ package chapter2;
 
 public class Alice {
 
-    private void eatCake(){
+    private int height;
+    private Cake cake;
+    private Tea tea;
+    private Mushroom mushroom;
+    private Fan fan;
 
+    public Alice() {
+        this.height = 130;
+    }
+
+    private void eatCake(){
+        height *= 2;
     }
 
     private void drinkTea(){
-
+        height = 24;
     }
 
     private void useFan(){
-
+        height -= 20;
     }
 
     private void eatMushroomToBeTall(){
-
+        height += 10;
     }
 
     private void eatMushroomToBeSmall(){
+        height -= 10;
 
     }
 }
