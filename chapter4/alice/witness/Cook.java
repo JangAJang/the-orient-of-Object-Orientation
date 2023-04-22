a704aed50f05eb7e9e1d9e1644aa83ac8bbf2a54
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-22 16:56:33 +0900

    [feat] 증인의 역할을 하는 Alice, Cook, MadHatter 객체 생성 및 이름을 말하고, 증언을 하게 하는 기능 구현

---
+++ chapter4/alice/witness/Cook.java
@@ -0,0 +1,20 @@
+package chapter4.alice.witness;
+
+public class Cook implements Witness{
+
+    private final String testimony;
+
+    public Cook(String testimony) {
+        this.testimony = testimony;
+    }
+
+    @Override
+    public String tellWhatTheyKnow() {
+        return testimony;
+    }
+
+    @Override
+    public String getName() {
+        return "요리사";
+    }
+}
